cff348b9dfeb8713b4f6f787aa76def934b35735
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-02-23 20:13:16 +0330

    fix a problem

--- src/Components/header/menu.jsx
+++ src/Components/header/menu.jsx
@@ -1,65 +1,79 @@
 import React from 'react';
+import {isEmpty} from 'lodash';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import {Link , withRouter} from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import {useSelector , useDispatch} from 'react-redux';
+import {deleteUser} from '../../Action/userAction';
 
 const SimpleMenu = ({location})=> {
   const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const userName = useSelector(state => state.User);
+  const dis = useDispatch();
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
+  
   const handleClose = () => {
     setAnchorEl(null);
   };
-
-  let buttonName = "Accounts";
-  if(userName.fullname)
-  {
-    buttonName = userName.fullname;
-  }
-  else{
-    if(location.pathname === "/register")
-    { 
-      buttonName = "Register";
-    }
-    if(location.pathname === "/login")
-    {
-      buttonName = "Login"
-    }
-  }
   
+  const userInfo = useSelector(state => state.User);
 
-  return (
-    <div>
-      <Button className="menu-btn" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        {buttonName}
-        &nbsp;
-        <i className="fas fa-caret-down"></i>
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>
-          <Link to="/login">Login</Link>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Link to="/register">Register</Link>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Link to="/">Logout</Link>  
-        </MenuItem>
-      </Menu>
-    </div>
+  return(
+
+    <React.Fragment>
+      {isEmpty(userInfo) ? (
+        <div>
+          <Button className="menu-btn" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            Account
+            &nbsp;
+            <i className="fas fa-caret-down"></i>
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link to="/login">Login</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to="/register">Register</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to="/">Logout</Link>
+            </MenuItem>
+          </Menu>
+        </div>
+      ) : (
+        <div>
+        <Button className="menu-btn" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+          {userInfo.fullname}
+          &nbsp;
+          <i className="fas fa-caret-down"></i>
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <Link to="/">Setting</Link>
+          </MenuItem>
+          <MenuItem onClick={()=> dis(deleteUser())}>
+            <Link to="/">Logout</Link>
+          </MenuItem>
+        </Menu>
+      </div>
+      )}
+    </React.Fragment>
+
   );
 }
 
