139da09930b1a273b4452c84a8d9dcb0bee42218
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-04-12 03:29:39 +0430

    user api register / 99%

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,28 +1,66 @@
 const UserModel = require('../Models/UserModel');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 async function registerUser(req , res)
 {
     try{
-        const {fullname , email , password} = req.body;
-        if(!fullname || !email || !password)
-        {
-            return res.json({
-                status: 406,
-                msg: 'Please complete the required items'
-            });
-        }
-        else
-        {
-            const User = await UserModel({
-                fullname,
-                email,
-                password
-            });
+        const {fullname , email , password , password2} = req.body;
+        let errors = [];
 
-            await User.save()
-            .then(() => res.json({status: 201}))
-            .catch(() => res.json({status: 503}));
+        let find = await UserModel.find({email});
+        if(find.length > 0)
+        {
+            errors.push({msg: 'this username/email has been used'});
+            return res.json({
+                msg: 'this username/email has been used',
+                status: 401
+            })
+        }
+        else{
+            //Check required fields
+            if(!fullname || !email || !password || !password2)
+            {
+                errors.push({msg: 'Please complete the required items'});
+                return res.json({
+                    status: 406,
+                    msg: 'Please complete the required items'
+                });
+            }
+
+            //Check password match
+            if(password !== password2)
+            {
+                errors.push({msg: 'Passwords do not match'});
+                return res.json({
+                    msg: 'Passwords do not match',
+                    status: 406
+                });
+            }
+
+            //Check passwords length
+            if(password.length < 8)
+            {
+                errors.push({msg: 'Passwords should be at least 6 characters'})
+                return res.json({
+                    msg: 'Passwords should be at least 6 characters',
+                    status: 406
+                })
+            }
+
+            if(errors.length > 0) return null
+            else{
+                let hashPass = await bcrypt.hash(password , 10);
+                const User = await UserModel({
+                    fullname,
+                    email,
+                    password: hashPass
+                });
+    
+                await User.save()
+                .then(() => res.json({status: 201}))
+                .catch(() => res.json({status: 503}));
+            }
         }
     }catch(err)
     {
